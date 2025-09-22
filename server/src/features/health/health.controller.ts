d93152bc234cd5dd46aa615c6a39c80687462651
CristelG <cristel.ganna@outlook.com>
2025-09-22 15:16:57 +0200

    added degraded connection response to health endpoints

--- server/src/features/health/health.controller.ts
+++ server/src/features/health/health.controller.ts
@@ -2,11 +2,12 @@ import type { Request, Response } from "express";
 import { healthCheck } from "./health.service.js";
 
 export const getHealth = async (req: Request, res: Response) => {
-    try{
-        const health = await healthCheck();
-        res.status(200).json(health)
-    }catch(err){
-        res.status(503).json('Health check failed')
-        throw err;
-    }
-}
+  try {
+    const health = await healthCheck();
+    if (health.status === "degraded") return res.status(503).json(health);
+    res.status(200).json(health);
+  } catch (err) {
+    res.status(503).json("Health check failed");
+    throw err;
+  }
+};
--- server/src/features/health/health.service.ts
+++ server/src/features/health/health.service.ts
@@ -3,7 +3,21 @@ import prisma from "../../shared/prisma/index.js";
 
 export const healthCheck = async () => {
   try {
-    await prisma.$queryRaw`SELECT 1`;
+    const startTime = Date.now();
+    const query = await prisma.$queryRaw`SELECT 1`;
+    // await new Promise((resolve) => setTimeout(resolve, 6000));
+    const elapsed = Date.now() - startTime;
+    
+    if (elapsed > 5000) {
+      return {
+        status: "degraded",
+        timestamp: new Date(),
+        version: "1.0.0",
+        database: "connected",
+        uptime: process.uptime(),
+        timeElapsed: elapsed
+      };
+    }
 
     return {
       status: "healthy",
@@ -11,10 +25,10 @@ export const healthCheck = async () => {
       version: "1.0.0",
       database: "connected",
       uptime: process.uptime(),
+      timeElapsed: elapsed
     };
   } catch (err: any) {
-
-    // if (err?.code) throw PrismaErrors.code(err.code);
+    if (err?.code) throw PrismaErrors.code(err.code);
     throw err;
   }
 };
